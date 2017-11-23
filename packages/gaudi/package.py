9be0b56b86ac7351d3719762b0c685fc09d463e2
Javier Cervantes Villanueva <jcervant@pcphsft109.cern.ch>
2017-11-23 10:24:13 +0100

    New package overwritting hep-spack: gaudi

---
+++ packages/gaudi/package.py
@@ -0,0 +1,21 @@
+from spack import *
+
+class Gaudi(CMakePackage):
+    """Gaudi framework."""
+    homepage = "https://gaudi.cern.ch"
+    url      = "https://gaudi.cern.ch"
+
+    version('v27r1', git='https://gitlab.cern.ch/gaudi/Gaudi.git', tag='v27r1')
+    version('v28r0', git='https://gitlab.cern.ch/gaudi/Gaudi.git', tag='v28r0')
+    version('v28r1', git='https://gitlab.cern.ch/gaudi/Gaudi.git', tag='v28r1')
+
+    depends_on("python")
+    depends_on("root")
+    depends_on("py-qmtest")
+    depends_on("clhep")
+    depends_on("boost")
+    depends_on("cppunit")
+    depends_on("aida")
+    depends_on("intel-tbb")
+    depends_on("gperftools")
+    depends_on("heppdt")
