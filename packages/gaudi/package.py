c531245399937f92ae526263c0f40aba0929a3dc
Javier Cervantes Villanueva <javier.cervantes.villanueva@cern.ch>
2018-01-16 10:11:11 +0100

    Add v29v2 version to gaudi

--- packages/gaudi/package.py
+++ packages/gaudi/package.py
@@ -9,6 +9,7 @@ class Gaudi(CMakePackage):
     version('v28r0', git='https://gitlab.cern.ch/gaudi/Gaudi.git', tag='v28r0')
     version('v28r1', git='https://gitlab.cern.ch/gaudi/Gaudi.git', tag='v28r1')
     version('v28r2', git='https://gitlab.cern.ch/gaudi/Gaudi.git', tag='v28r2')
+    version('v29r2', git='https://gitlab.cern.ch/gaudi/Gaudi.git', tag='v29r2')
 
     depends_on("python")
     depends_on("root")
