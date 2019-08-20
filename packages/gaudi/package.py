186415ab3b1b8d1482f8ad03e5f8f665a0ec2f91
Javier Cervantes Villanueva <javier.cervantes.villanueva@cern.ch>
2019-08-20 16:03:46 +0200

    Define BINARY_TAG for gaudi

--- packages/gaudi/package.py
+++ packages/gaudi/package.py
@@ -1,4 +1,6 @@
 from spack import *
+import llnl.util.tty as tty
+
 
 class Gaudi(CMakePackage):
     """Gaudi framework."""
@@ -58,12 +60,34 @@ class Gaudi(CMakePackage):
         spec = self.spec
         options = []
         if '^boost@1.69:' in spec:
-             options = [
-                 # Otherwise find_package(Boost) finds the wrong directories
-                 '-DBoost_NO_BOOST_CMAKE=TRUE',
-             ]
+            options = [
+                # Otherwise find_package(Boost) finds the wrong directories
+                '-DBoost_NO_BOOST_CMAKE=TRUE',
+            ]
         return options
 
     def setup_environment(self, spack_env, run_env):
         if 'xz' in self.spec:
             spack_env.prepend_path('LD_LIBRARY_PATH', self.spec['xz'].prefix.lib)
+
+        # Gaudi automatically detects the processor if BINARY_TAG is not defined
+        # in the environment. This leads to an error detecting a 'broadwell'
+        # platform instead of 'x86_64'. This solves this issue.
+        import platform
+        binary_tag=["x86_64"]
+        if "CentOS" in platform.linux_distribution():
+            binary_tag.append("centos7")
+        else:
+            binary_tag.append("slc6")
+
+        compiler_labels = {
+            "gcc@8.2.0": "gcc8",
+            "gcc@6.2.0": "gcc62"
+        }
+
+        binary_tag.append(compiler_labels[str(self.compiler.spec)])
+        binary_tag.append("opt")
+
+        spack_env.set('BINARY_TAG', "-".join(binary_tag))
+        msg="Defining the following environment variable: BINARY_TAG="+"-".join(binary_tag)
+        tty.msg(msg)
