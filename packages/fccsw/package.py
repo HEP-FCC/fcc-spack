3a12b0245a072c6abf32e4a30a98920c4666b231
Javier Cervantes Villanueva <javier.cervantes.villanueva@cern.ch>
2019-09-13 17:20:33 +0200

    set up geant4 datasets in fccsw

--- packages/fccsw/package.py
+++ packages/fccsw/package.py
@@ -24,7 +24,8 @@
 ##############################################################################
 
 from spack import *
-
+import llnl.util.tty as tty
+import subprocess
 
 class Fccsw(CMakePackage):
     """software framework of the FCC project"""
@@ -38,11 +39,17 @@ class Fccsw(CMakePackage):
     version('0.5', 'f2c849608ac1ab175f432a5e55dbe673')
 
     variant('lcg', default=True, description="Installed against an LCG Release")
+    variant('cxxstd',
+            default='17',
+            values=('14', '17'),
+            multi=False,
+            description='Use the specified C++ standard when building.')
 
     depends_on('acts-core')
     depends_on('clhep')
     depends_on('dd4hep')
     depends_on('delphes')
+    depends_on('gl2ps')
     depends_on('fastjet')
 
     # LCG Releases built with gcc7 use C++17
@@ -73,7 +80,51 @@ class Fccsw(CMakePackage):
     patch('permissions.patch', when='@0.9')
     patch('ddeve.patch', when='@0.9 ^dd4hep@01-08')
 
+    def cmake_args(self):
+        args = []
+        # C++ Standard
+        args.append('-DCMAKE_CXX_STANDARD=%s' % self.spec.variants['cxxstd'].value)
+        return args
+    
     def setup_environment(self, spack_env, run_env):
         # Need to explicitly add DD4hep libs to the LD_LIBRARY_PATH since
         # some cmake files (MakeGaudiMap.cmake) only rely on this variable
         spack_env.prepend_path('LD_LIBRARY_PATH', self.spec['dd4hep'].prefix.lib)
+
+        # Gaudi automatically detects the processor if BINARY_TAG is not defined
+        # in the environment. This leads to an error detecting a 'broadwell'
+        # platform instead of 'x86_64'. This solves this issue.
+        import platform
+        binary_tag=["x86_64"]
+        tty.msg(platform.linux_distribution()[0])
+        if "CentOS" in platform.linux_distribution()[0]:
+            binary_tag.append("centos7")
+        else:
+            binary_tag.append("slc6")
+
+        compiler_labels = {
+            "gcc@8.2.0": "gcc8",
+            "gcc@8.3.0": "gcc8",
+            "gcc@6.2.0": "gcc62"
+        }
+
+        binary_tag.append(compiler_labels[str(self.compiler.spec)])
+        binary_tag.append("opt")
+
+        spack_env.set('BINARY_TAG', "-".join(binary_tag))
+        msg="Defining the following environment variable: BINARY_TAG="+"-".join(binary_tag)
+        tty.msg(msg)
+	
+        # Set up Geant4 datasets
+	# ToFix: This should be set by Geant4 itself
+	geant4 = self.spec['geant4']
+	geant4config = join_path(geant4.prefix.bin, "geant4-config")
+        process_pipe = subprocess.Popen([geant4config, "--datasets"],
+                                        stdout=subprocess.PIPE)
+        result_datasets = process_pipe.communicate()[0]
+        for line in result_datasets.rstrip("\n").split("\n"):
+            dataset = line.split()
+            # Format: directory name, environment variable, directory path
+            g4variable = dataset[1]
+            g4datapath = dataset[2]
+            spack_env.set(g4variable, g4datapath)
