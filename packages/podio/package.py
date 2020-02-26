fc7955b132f17a4741faed1ee99e2806acdd03d5
Valentin Volkl <valentin.volkl@cern.ch>
2020-02-26 11:36:49 +0100

    update-podio

--- packages/podio/package.py
+++ packages/podio/package.py
@@ -29,23 +29,35 @@ class Podio(CMakePackage):
     """PODIO, or plain-old-data I/O, is a C++ library to support the creation
     and handling of data models in particle physics."""
 
-    homepage = "https://github.com/HEP-FCC/podio"
-    url      = "https://github.com/HEP-FCC/podio/archive/v0.9.1.tar.gz"
+    homepage = "https://github.com/AIDASOFT/podio"
+    url      = "https://github.com/AIDASOFT/podio"
 
-    version('0.9.1', '9e2b8cf6d2be46d74471ae4857bd7191', preferred=True)
-    version('0.8', '07bf090649fc8e3b94e348e6b7fb8c7e')
-    version('0.7', '79e11c02c8d588f7b1dfc39b7de4eed9')
-    version('0.6', '1a1d2aa70fc16cce372ce10c3612c32d')
-    version('develop', git='https://github.com/HEP-FCC/podio.git', branch='master')
+    version('00-09-02', sha256='8234d1b9636029124235ef81199a1220968dcc7fdaeab81cdc96a47af332d240', preferred=True)
+    version('00-09', sha256='3cde67556b6b76fd2d004adfaa3b3b6173a110c0c209792bfdb5f9353e21076f')
+    version('00-08', sha256='9d035a7f5ebfae5279a17405003206853271af692f762e2bac8e73825f2af327')
+    version('develop', git='https://github.com/AIDASOFT/podio.git', branch='master')
 
     variant('build_type', default='Release',
             description='The build type to build',
             values=('Debug', 'Release'))
 
+    variant('cxxstd',
+            default='17',
+            values=('14', '17'),
+            multi=False,
+            description='Use the specified C++ standard when building.')
+
     depends_on('cmake')
     depends_on('root@6.08.06:')
     depends_on('python@2.7:')
     depends_on('py-pyyaml')
+    depends_on('tbb')
+
+    def cmake_args(self):
+        args = []
+        # C++ Standard
+        args.append('-DCMAKE_CXX_STANDARD=%s' % self.spec.variants['cxxstd'].value)
+        return args
 
     # in LCG_96 ROOT is installed with an external xz rather than the builtin,
     # so the genreflex binary needs to find it.
@@ -60,3 +72,10 @@ class Podio(CMakePackage):
 
     def setup_dependent_environment(self, spack_env, run_env, dspec):
         spack_env.set('PODIO', self.prefix)
+
+    def setup_environment(self, spack_env, run_env):
+	# needed for genreflex
+    	spack_env.prepend_path('LD_LIBRARY_PATH', self.spec['root'].prefix.lib)
+    	run_env.prepend_path('LD_LIBRARY_PATH', self.spec['root'].prefix.lib)
+        if 'xz' in self.spec:
+            spack_env.prepend_path('LD_LIBRARY_PATH', self.spec['xz'].prefix.lib)
