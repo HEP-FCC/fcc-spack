3dad36389584358dbd6ff6e7565f8e61272cc9fd
Valentin Volkl <valentin.volkl@cern.ch>
2020-04-01 11:50:37 +0200

    fix tabs

--- packages/fcc-edm-legacy/package.py
+++ packages/fcc-edm-legacy/package.py
@@ -1,34 +1,11 @@
-##############################################################################
-# Copyright (c) 2013-2018, Lawrence Livermore National Security, LLC.
-# Produced at the Lawrence Livermore National Laboratory.
-#
-# This file is part of Spack.
-# Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
-# LLNL-CODE-647188
-#
-# For details, see https://github.com/spack/spack
-# Please also see the NOTICE and LICENSE files for our notice and the LGPL.
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License (as
-# published by the Free Software Foundation) version 2.1, February 1999.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
-# conditions of the GNU Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public
-# License along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-##############################################################################
+
 from spack import *
 
 
 class FccEdmLegacy(CMakePackage):
-    """FCCEDM"""
+    """FCCEDM Legacy"""
 
-    """Event data model of FCC"""
+    """Event data model of FCC - Legacy types"""
 
     homepage = "https://github.com/HEP-FCC/fcc-edm-legacy"
     url      = "https://github.com/HEP-FCC/fcc-edm-legacy/archive/v0.1.0.zip"
@@ -61,11 +38,11 @@ class FccEdmLegacy(CMakePackage):
             make("test", "CTEST_OUTPUT_ON_FAIL=1")
     
     def setup_environment(self, spack_env, run_env):
-	# needed for genreflex
-    	spack_env.prepend_path('LD_LIBRARY_PATH', self.spec['root'].prefix.lib)
-    	run_env.prepend_path('LD_LIBRARY_PATH', self.spec['root'].prefix.lib)
+      # needed for genreflex
+      spack_env.prepend_path('LD_LIBRARY_PATH', self.spec['root'].prefix.lib)
+      run_env.prepend_path('LD_LIBRARY_PATH', self.spec['root'].prefix.lib)
         if 'xz' in self.spec:
-            spack_env.prepend_path('LD_LIBRARY_PATH', self.spec['xz'].prefix.lib)
+          spack_env.prepend_path('LD_LIBRARY_PATH', self.spec['xz'].prefix.lib)
 
     def setup_dependent_environment(self, spack_env, run_env, dspec):
         spack_env.set('FCCEDMLEGACY', self.prefix)
--- packages/fcc-edm/package.py
+++ packages/fcc-edm/package.py
@@ -1,30 +1,5 @@
-##############################################################################
-# Copyright (c) 2013-2016, Lawrence Livermore National Security, LLC.
-# Produced at the Lawrence Livermore National Laboratory.
-#
-# This file is part of Spack.
-# Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
-# LLNL-CODE-647188
-#
-# For details, see https://github.com/llnl/spack
-# Please also see the LICENSE file for our notice and the LGPL.
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License (as
-# published by the Free Software Foundation) version 2.1, February 1999.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
-# conditions of the GNU Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public
-# License along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-##############################################################################
 
 from spack import *
-
 
 class FccEdm(CMakePackage):
     """Event data model of FCC"""
@@ -83,11 +58,11 @@ class FccEdm(CMakePackage):
             make("test", "CTEST_OUTPUT_ON_FAIL=1")
 
     def setup_environment(self, spack_env, run_env):
-	# needed for genreflex
-    	spack_env.prepend_path('LD_LIBRARY_PATH', self.spec['root'].prefix.lib)
-    	run_env.prepend_path('LD_LIBRARY_PATH', self.spec['root'].prefix.lib)
+      # needed for genreflex
+      spack_env.prepend_path('LD_LIBRARY_PATH', self.spec['root'].prefix.lib)
+      run_env.prepend_path('LD_LIBRARY_PATH', self.spec['root'].prefix.lib)
         if 'xz' in self.spec:
-            spack_env.prepend_path('LD_LIBRARY_PATH', self.spec['xz'].prefix.lib)
+          spack_env.prepend_path('LD_LIBRARY_PATH', self.spec['xz'].prefix.lib)
 
     def setup_dependent_environment(self, spack_env, run_env, dspec):
         spack_env.set('FCCEDM', self.prefix)
