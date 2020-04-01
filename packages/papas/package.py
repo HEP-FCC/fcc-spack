b9f6bdcc841dbe4bc96e4aa9ff69adcc7e29ba2a
Valentin Volkl <valentin.volkl@cern.ch>
2020-04-01 12:07:17 +0200

    further indentation fixes

--- packages/papas/package.py
+++ packages/papas/package.py
@@ -1,27 +1,4 @@
-##############################################################################
-# Copyright (c) 2013-2017, Lawrence Livermore National Security, LLC.
-# Produced at the Lawrence Livermore National Laboratory.
-#
-# This file is part of Spack.
-# Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
-# LLNL-CODE-647188
-#
-# For details, see https://github.com/spack/spack
-# Please also see the NOTICE and LICENSE files for our notice and the LGPL.
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License (as
-# published by the Free Software Foundation) version 2.1, February 1999.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
-# conditions of the GNU Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public
-# License along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-##############################################################################
+
 from spack import *
 
 
@@ -46,7 +23,7 @@ class Papas(CMakePackage):
             default='14',
             values=('14', '17'),
             multi=False,
-	    description='Use the specified C++ standard when building.')
+      description='Use the specified C++ standard when building.')
 
     depends_on('fcc-physics')
     depends_on('fcc-edm')
@@ -55,12 +32,10 @@ class Papas(CMakePackage):
 
 
     def cmake_args(self):
-	args = []
-
-	# C++ Standard
-        args.append('-DCMAKE_CXX_STANDARD=%s' % self.spec.variants['cxxstd'].value)
-
-	return args
+      args = []
+      # C++ Standard
+      args.append('-DCMAKE_CXX_STANDARD=%s' % self.spec.variants['cxxstd'].value)
+      return args
 
     def setup_dependent_environment(self, spack_env, run_env, dspec):
         spack_env.set('PAPAS', self.prefix)
