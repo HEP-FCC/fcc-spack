3b7b185caca92ed837edc18ae5c1ad055433465c
Valentin Volkl <valentin.volkl@cern.ch>
2020-04-01 12:04:07 +0200

    further indentation fixes

--- packages/fcc-physics/package.py
+++ packages/fcc-physics/package.py
@@ -1,27 +1,3 @@
-##############################################################################
-# Copyright (c) 2013-2016, Lawrence Livermore National Security, LLC.
-# Produced at the Lawrence Livermore National Laboratory.
-#
-# This file is part of Spack.
-# Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
-# LLNL-CODE-647188
-#
-# For details, see https://github.com/llnl/spack
-# Please also see the LICENSE file for our notice and the LGPL.
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License (as
-# published by the Free Software Foundation) version 2.1, February 1999.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
-# conditions of the GNU Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public
-# License along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-##############################################################################
 
 from spack import *
 
@@ -57,11 +33,11 @@ class FccPhysics(CMakePackage):
     depends_on('xz', when='^root@6.16:')
 
     def setup_environment(self, spack_env, run_env):
-    	spack_env.prepend_path('LD_LIBRARY_PATH', self.spec['root'].prefix.lib)
-    	run_env.prepend_path('LD_LIBRARY_PATH', self.spec['root'].prefix.lib)
-        spack_env.set('HEPMC_PREFIX', self.spec['hepmc'].prefix)
-        if 'xz' in self.spec:
-            spack_env.prepend_path('LD_LIBRARY_PATH', self.spec['xz'].prefix.lib)
+      spack_env.prepend_path('LD_LIBRARY_PATH', self.spec['root'].prefix.lib)
+      run_env.prepend_path('LD_LIBRARY_PATH', self.spec['root'].prefix.lib)
+      spack_env.set('HEPMC_PREFIX', self.spec['hepmc'].prefix)
+      if 'xz' in self.spec:
+        spack_env.prepend_path('LD_LIBRARY_PATH', self.spec['xz'].prefix.lib)
 
     def setup_dependent_environment(self, spack_env, run_env, dspec):
         spack_env.set('FCCPHYSICS', self.prefix)
